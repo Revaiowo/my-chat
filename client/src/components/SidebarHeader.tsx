22359fd2000a77efa2d79d41a1dfaf69e4f8d869
Reaviowo <levitheackerman1@gmail.com>
2025-03-20 10:38:27 +0530

    Fixed logout route

--- client/src/components/SidebarHeader.tsx
+++ client/src/components/SidebarHeader.tsx
@@ -57,7 +57,7 @@ function SidebarHeader() {
 	const handleLogout = async () => {
 		try {
 			const res = await axios.post(
-				"http://localhost:5000/api/auth/logout",
+				"/api/auth/logout",
 				{},
 				{ withCredentials: true }
 			);
